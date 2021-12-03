5c1be47ac2776b0257df11113c5cefcead599c39
Anthony Lu <anthonylu@berkeley.edu>
2021-12-02 16:16:43 -0800

    backend changes

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const axios = require('axios')
+
+var cors = require('cors')
+var app = express()
+
+app.use(cors())
+const port = 3000 || process.env.PORT;
+
+const apiKey = '636a1bbe30c1423fbc94c278e16834d7';
+
+app.get('/gen-recipes', async (req, res) => {
+    const ing = await axios.get('https://api.spoonacular.com/recipes/findByIngredients?apiKey=' + apiKey + '&ingredients=' + req.query.ingredients + '&number=30');
+
+    res.json(ing.data);
+})
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
